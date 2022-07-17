111472abb7e30d7db6693363a237b99780f2a7d9
Mahmoud GadElrab <102475771+Mahmoud12501@users.noreply.github.com>
2022-07-17 17:42:30 +0200

    Update hi.py

--- hi.py
+++ hi.py
@@ -3,47 +3,47 @@ import schedule
 import time
 from win10toast import ToastNotifier
 
-# remander_dict={}
-# def to_dolist():
+remander_dict={}
+def to_dolist():
     
     
-#     number_task=int(input("how mant task do you want to record: "))
-#     for i in range(number_task):
-#         task_time=input(f"entert task{i+1} time: ").split(":")
-#         task_name=input(f"task{i+1} name: ").title
+    number_task=int(input("how mant task do you want to record: "))
+    for i in range(number_task):
+        task_time=input(f"entert task{i+1} time: ").split(":")
+        task_name=input(f"task{i+1} name: ").title
     
-#         h,m=map(int,task_time)
-#         task_time=datetime.time(h,m)
-#         print(task_time)
-#         print(type(task_time))
-#         remander_dict[task_time]=task_name  
-#     print(remander_dict)     
+        h,m=map(int,task_time)
+        task_time=datetime.time(h,m)
+        print(task_time)
+        print(type(task_time))
+        remander_dict[task_time]=task_name  
+    print(remander_dict)     
     
-# def rmander():  
-#     notvication=ToastNotifier() 
-#     for not_time , task in remander_dict.items():
-#         if not_time==datetime.datetime.now().time():
-#              print("yes")
-#              notvication.show_toast(
-#             task,
-#             f"Time of {task}",
-#             icon_path=None,
-#             duration=10,
-#             threaded=True)
-#         else:
-#             print("no")
+def rmander():  
+    notvication=ToastNotifier() 
+    for not_time , task in remander_dict.items():
+        if not_time==datetime.datetime.now().time():
+             print("yes")
+             notvication.show_toast(
+            task,
+            f"Time of {task}",
+            icon_path=None,
+            duration=10,
+            threaded=True)
+        else:
+            print("no")
 
-# to_dolist()
-# schedule.every(1).seconds.do(rmander)
-# while True:
+to_dolist()
+schedule.every(1).seconds.do(rmander)
+while True:
  
-#     # Checks whether a scheduled task
-#     # is pending to run or not
-#     schedule.run_pending()
-#     time.sleep(1)
+    # Checks whether a scheduled task
+    # is pending to run or not
+    schedule.run_pending()
+    time.sleep(1)
 
-x=datetime.time(2,3)
-print(x)
-y="hi".title()
-dict1={y:x}
-print(dict1)
+# x=datetime.time(2,3)
+# print(x)
+# y="hi".title()
+# dict1={y:x}
+# print(dict1)
