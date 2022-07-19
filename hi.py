442409bc4de8a6e3b4854147f5b5001c1bd054a4
Mahmoud12501 <mahmoudmuhmmed2001@gmail.com>
2022-07-19 20:16:15 +0200

    to_do_list_rmander

--- hi.py
+++ hi.py
@@ -7,79 +7,44 @@ from win10toast import ToastNotifier
 
 
 remander_dict={}
-# def to_dolist():
+def to_dolist():
     
     
-#     number_task=int(input("how mant task do you want to record: "))
-#     for i in range(number_task):
-#         task_time=input(f"entert task{i+1} time: ").split(":")
-#         task_name=input(f"task{i+1} name: ").title
-    
-#         h,m=map(int,task_time)
-#         task_time=datetime.time(h,m)
-#         print(task_time)
-#         print(type(task_time))
-#         remander_dict[task_time]=task_name  
-#         x=remander_dict[task_time]  
-    
-# def rmander():  
-#     notvication=ToastNotifier() 
-#     for not_time , task in remander_dict.items():
-#         if not_time==datetime.datetime.now().time():
-#              print("yes")
-#              notvication.show_toast(
-#             task,
-#             f"Time of {task}",
-#             icon_path=None,
-#             duration=10,
-#             threaded=True)
-#         else:
-#             print("no")
-
-# schedule.every(1).seconds.do(rmander)
-# while True:
- 
-#     # Checks whether a scheduled task
-#     # is pending to run or not
-#     schedule.run_pending()
-#     time.sleep(1)
-
-
-all_reminder=[]
-def take_task():
     number_task=int(input("how mant task do you want to record: "))
-    for i in range(number_task):
-        all_reminder.append([])
 
+    # take task and thier time 
+    for i in range(number_task):
         task_time=input(f"entert task{i+1} time: ").split(":")
-        task_name=input(f"task{i+1} name: ").title()
-            
+        task_name=input(f"task{i+1} name: ").title
+    
         h,m=map(int,task_time)
-        task_time=datetime.time(h,m)
-        for j in range(1):
-            all_reminder[i].append(task_time)
-            all_reminder[i].append(task_name)
-        
+        task_time=datetime.time(h,m,0,0)
+        print(task_time)
+        print(type(task_time))
+
+        # store task name as a key in dict and time as a value
+        remander_dict[task_name]=task_time  
+        x=remander_dict[task_name]  
+    
 def rmander():  
     notvication=ToastNotifier() 
-    tim = time.localtime()
 
-    current_time = time.strftime("%H:%M:%S", tim)
-    for i in all_reminder:
-        # x=all_reminder[i][0]
-        # print(x)
-        if datetime.datetime.now().hour==datetime.datetime.now().hour:
+    for   task,not_time  in remander_dict.items():
+        
+        if not_time==datetime.datetime.now().time():
              print("yes")
              notvication.show_toast(
-            all_reminder[i][1],
-            f"Time of { all_reminder[i][1]}",
+            task,
+            f"Time of {task}",
             icon_path=None,
-            duration=10,
+            duration=20,
             threaded=True)
         else:
+            print(not_time)
+            print(datetime.datetime.now().time())
             print("no")
 
-take_task()
+to_dolist()
 schedule.every(1).seconds.do(rmander)
 while True:
  
@@ -87,4 +52,3 @@ while True:
     # is pending to run or not
     schedule.run_pending()
     time.sleep(1)
-
